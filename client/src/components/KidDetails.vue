94821826e5d8ed93830d6da9ad1cbc85a0de8b5f
Erez Vishnia <erezvish@gmail.com>
2017-07-06 14:16:54 +0300

    toggle is active on the card

--- client/src/components/KidDetails.vue
+++ client/src/components/KidDetails.vue
@@ -1,48 +1,63 @@
 <template>
-  <section class="kid-details">
+  <section :class="classObject" @click="toggleIsPresent" >
     <div class="kid-img">
       <img :src="kid.imgUrl">
     </div>
     <div class="properties">
       <div class="card-header hr">
-        <h2>{{`${kid.firstName} ${kid.lastName}`}}  </h2>
+        <h2>{{`${kid.firstName} ${kid.lastName}`}} </h2>
         <i class="fa fa-pencil" aria-hidden="true"></i>
       </div>
       <ul>
-        <li>status: 
-          <span class="kid-present" v-show="kid.isPresent"> In Class </span> 
-          <span class="kid-away" v-show="!kid.isPresent"> NOT IN CLASS </span></li>
+        <li>status:
+          <span class="kid-present" v-show="kid.isPresent"> In Class </span>
+          <span class="kid-away" v-show="!kid.isPresent"> NOT IN CLASS </span>
+        </li>
         <li class="hr">last seen:</li>
       </ul>
       <div class="msg-parent">
         <el-input placeholder="Message parent" v-model="inputMsgParent"></el-input>
-        <el-button type="success"><i class="fa fa-paper-plane" aria-hidden="true"></i></el-button>
+        <el-button type="success">
+          <i class="fa fa-paper-plane" aria-hidden="true"></i>
+        </el-button>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import store from '../store'
 export default {
   name: 'kid-details',
   props: ['kid'],
   data() {
     return {
       inputMsgParent: '',
+      classObject: {
+        'kid-details': true,
+        'mark-present': this.kid.isPresent,
+        'mark-absent': !this.kid.isPresent        
+      }
     }
   },
-    created() {
+  created() {
     console.log('kid details:', this.kid)
   },
+  methods: {
+    toggleIsPresent() {
+      this.$store.dispatch({
+        type: 'togglePresent',
+        kid: this.kid
+      })
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
-  // * {
-  //   outline: 1px solid green;
-  // }
-
+// * {
+//   outline: 1px solid green;
+// }
 .properties {
   padding: 0 0.5em;
 
@@ -50,22 +65,21 @@ export default {
     // font-size: 1.8em;
   }
 }
+
 .kid-details {
   background: white;
   border-radius: 1em;
   display: flex;
   flex-direction: column;
-  width: 250px;
-  // height: 350px;
+  width: 250px; // height: 350px;
   margin: 1em;
-  border: 0.2em solid rgba(51, 51, 51,0.2);
+  border: 0.2em solid rgba(51, 51, 51, 0.2);
+  cursor: pointer;
 
   & ul {
     list-style-type: none;
     padding: 0;
-    text-align: left;
-    // align-self: flex-start
-
+    text-align: left; // align-self: flex-start
     & li {
       padding: 0.5em 0;
     }
@@ -79,11 +93,11 @@ export default {
 }
 
 .kid-img {
- border: {
-  top-left-radius: 0.1em;
-  top-right-radius: 0.1em;
-  bottom: 0.1em solid rgba(0,0,0,0.07);
- } 
+  border: {
+    top-left-radius: 0.1em;
+    top-right-radius: 0.1em;
+    bottom: 0.1em solid rgba(0, 0, 0, 0.07);
+  }
   background: lightgray;
   height: 150px;
   width: 100%;
@@ -93,14 +107,21 @@ export default {
   }
 }
 
+.mark-present {
+  border: 2px solid green;
+}
+.mark-absent {
+  border: 2px solid red;
+}
+
 .msg-parent {
   display: flex;
   justify-content: space-between;
-  margin: 1.2em 0 ;
+  margin: 1.2em 0;
 }
 
 .hr {
   margin: 0.5em 0;
-  border-bottom: 1px solid rgba(0,0,0,0.1);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 }
 </style>
