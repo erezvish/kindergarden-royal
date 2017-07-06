d33d46589db71b9b59452a534bdfb7b1c0625061
Erez Vishnia <erezvish@gmail.com>
2017-07-06 17:33:00 +0300

    new filter files and getters

---
+++ client/src/components/FilterCmp.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <el-input placeholder="Search" icon="search" v-model="searchInput" @input=filterKids>
+        </el-input>
+    </section>
+</template>
+
+<script>
+
+export default {
+    name: 'filter-cmp',
+    data() {
+        return {
+            searchInput: ''
+        }
+    },
+    methods: {
+        filterKids() {
+            console.log('filtering!')
+            this.$store.dispatch({
+                type: 'filterKids',
+                text: this.searchInput
+            })
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
