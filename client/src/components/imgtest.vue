905158e5804cf5c423cf632550a33b0ba385c454
Meir <pechtalt@gmail.com>
2017-07-19 17:09:08 +0300

    looks like its working

---
+++ client/src/components/imgtest.vue
@@ -0,0 +1,26 @@
+<template>
+  <base64-upload class="user"
+    :imageSrc="imgUrl"
+    :imageStyle="{'border-radius': '50%'}"
+    @change="onChangeImage"></base64-upload>
+</template>
+<script>
+import Base64Upload from './Base64Upload.vue'
+export default {
+  components: {
+    Base64Upload
+  },
+  methods: {
+    onChangeImage(file) {
+      console.log(file)
+    }
+  }
+}
+</script>
+
+<style>
+.user {
+  width: 128px;
+  height: 128px;
+}
+</style>
